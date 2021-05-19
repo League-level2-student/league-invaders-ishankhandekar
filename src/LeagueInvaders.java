a63c9d1b9da5d630f86a83620cc2ca745163ae76
ishankhandekar <66933296+ishankhandekar@users.noreply.github.com>
2021-05-18 17:00:27 -0700

    5/18/2021

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,19 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+	JFrame j;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
 
+	public static void main(String[] args) {
+
+	}
+
+	public LeagueInvaders(JFrame j) {
+		this.j = j;
+	}
+	
+	public void setup() {
+		
+	}
 }
